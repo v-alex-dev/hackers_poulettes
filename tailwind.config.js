6c3781d0e53ff91f978ce00b78e8600a9c649d31
Alexandre <vensalex1991@gmail.com>
2023-11-07 14:45:41 +0100

    bug fix

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.php"],
+  theme: {
+    extend: {},
+    colors:{
+      'green':'#0d8187',
+      'white':'#FFF',
+      'dark':'#303030',
+      'grey':'#D9D9D9'
+    },
+  },
+  plugins: [],
+}
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9e4de5dee48e1f08a21b1523f2c24d3c2691a2fe',
+        'name' => 'vendor_name/hackers_poulettes',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.1',
+            'version' => '6.8.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'e88da8d679acc3824ff231fdc553565b802ac016',
+            'dev_requirement' => false,
+        ),
+        'vendor_name/hackers_poulettes' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9e4de5dee48e1f08a21b1523f2c24d3c2691a2fe',
+            'dev_requirement' => false,
+        ),
+    ),
+);
